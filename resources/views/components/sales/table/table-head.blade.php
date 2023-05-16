241a56fdab17471d47a40606678044247645a291
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-05-17 00:27:28 +0800

    updated table

--- resources/views/components/sales/table/table-head.blade.php
+++ resources/views/components/sales/table/table-head.blade.php
@@ -1,9 +1,29 @@
 <thead>
     <tr>
-        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
-        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Function</th>
-        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
-        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Employed</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Sales Date</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Page</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">CSR Name</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer Name</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Number</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Address Landmark</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Main Item</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">SKU 1</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">SKU 2</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">SKU 3</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">SKU 4</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Upseller</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Upsell Item</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Price</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Upsell Price</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Final Price</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Notes</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Call & Text Status</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Shipper</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Courier</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tracking Number</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">POS</th>
+        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">RTS Tracking Number</th>
         <th class="text-secondary opacity-7"></th>
     </tr>
 </thead>
