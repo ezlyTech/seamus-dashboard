a41cc1f3502296dffbf3e68c593d2818dfcec828
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-05-24 23:14:39 +0800

    added add-modal

---
+++ resources/views/components/orders/crud/add-modal.blade.php
@@ -0,0 +1,166 @@
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addModalLabel">Add New Order</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <form>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-date-input" class="form-control-label">Sales Date</label>
+                        <input class="form-control" type="date" value="2018-11-23" id="example-date-input">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Customer Name</label>
+                        <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Page</label>
+                        <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-number-input" class="form-control-label">Number</label>
+                        <input class="form-control" type="number" value="23" id="example-number-input">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">CSR Name</label>
+                        <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">Address Landmark</label>
+                        <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Main Item</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                          <label for="example-text-input" class="form-control-label">Upseller</label>
+                          <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Shipper</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Status</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="example-number-input" class="form-control-label">SKU 1</label>
+                            <input class="form-control" type="number" value="23" id="example-number-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                          <label for="example-text-input" class="form-control-label">Upsell Item</label>
+                          <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Courier</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Call & Text Status</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="example-number-input" class="form-control-label">SKU 2</label>
+                            <input class="form-control" type="number" value="23" id="example-number-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                        <label for="example-number-input" class="form-control-label">Price</label>
+                        <input class="form-control" type="number" value="23" id="example-number-input">
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">POS</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="example-number-input" class="form-control-label">Tracking #</label>
+                            <input class="form-control" type="number" value="23" id="example-number-input">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="form-group">
+                            <label for="example-number-input" class="form-control-label">SKU 3</label>
+                            <input class="form-control" type="number" value="23" id="example-number-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                      <div class="form-group">
+                        <label for="example-number-input" class="form-control-label">Upsell Price</label>
+                        <input class="form-control" type="number" value="23" id="example-number-input">
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="example-text-input" class="form-control-label">Notes</label>
+                            <input class="form-control" type="text" value="John Snow" id="example-text-input">
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="example-number-input" class="form-control-label">RTS Tracking #</label>
+                            <input class="form-control" type="number" value="23" id="example-number-input">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn bg-gradient-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+</div>
