0e2cace57eda2753bc10290b0b751e8d8c703d78
Eliz Marie Manalo <tech.ezly@gmail.com>
2023-06-20 23:10:04 +0800

    added dump context for general page

--- app/Http/Livewire/General.php
+++ app/Http/Livewire/General.php
@@ -4,10 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\Sales;
+use App\Models\Status;
+
 class General extends Component
 {
     public function render()
     {
-        return view('livewire.general');
+        return view('livewire.general', [
+            'statuses'=>Status::orderBy('status_name', 'asc')->get()
+        ]);
     }
 }
--- resources/views/livewire/general.blade.php
+++ resources/views/livewire/general.blade.php
@@ -1,3 +1,124 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
-</div>
+<main class="main-content">
+    <div class="container-fluid py-2">
+        <div class="row">
+            <div class="col-md-3 mt-4">
+                <div class="card">
+                    <div class="card-header pb-0 p-3">
+                        <div class="d-flex justify-content-between">
+                            <div class="col-md-6 d-flex align-items-center">
+                                <h6 class="mb-0">Statuses</h6>
+                            </div>
+                            <div class="">
+                                <button class="btn btn-outline-primary btn-sm mb-0">Add New</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-3 pb-0">
+                        <ul class="list-group">
+                            @foreach ($statuses as $status)
+                                <li class="list-group-item border-0 d-flex p-2 mb-2 bg-gray-100 border-radius-lg">{{ $status->status_name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 mt-4">
+                <div class="card">
+                    <div class="card-header pb-0 p-3">
+                        <div class="d-flex justify-content-between">
+                            <div class="col-md-6 d-flex align-items-center">
+                                <h6 class="mb-0">Call & Text Status</h6>
+                            </div>
+                            <div class="">
+                                <button class="btn btn-outline-primary btn-sm mb-0">Add New</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-3 pb-0">
+                        <ul class="list-group">
+                            @foreach ($statuses as $status)
+                                <li class="list-group-item border-0 d-flex p-2 mb-2 bg-gray-100 border-radius-lg">{{ $status->status_name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 mt-4">
+                <div class="card">
+                    <div class="card-header pb-0 p-3">
+                        <div class="d-flex justify-content-between">
+                            <div class="col-md-6 d-flex align-items-center">
+                                <h6 class="mb-0">Imports History</h6>
+                            </div>
+                            <div class="">
+                                <button class="btn btn-outline-primary btn-sm mb-0">View All</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-3 pb-0">
+                        <ul class="list-group">
+                            <li
+                                class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                <div class="d-flex flex-column">
+                                    <h6 class="mb-1 text-dark font-weight-bold text-sm">March, 01, 2020</h6>
+                                    <span class="text-xs">#CC-214589</span>
+                                </div>
+                                <div class="d-flex align-items-center text-sm">
+                                    {{-- $350 --}}
+                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
+                                            class="fas fa-file-pdf text-lg me-1"></i> CSV</button>
+                                </div>
+                            </li>
+                            <li
+                                class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                <div class="d-flex flex-column">
+                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">April, 05, 2020</h6>
+                                    <span class="text-xs">#FB-212562</span>
+                                </div>
+                                <div class="d-flex align-items-center text-sm">
+                                    {{-- $560 --}}
+                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
+                                            class="fas fa-file-pdf text-lg me-1"></i> CSV</button>
+                                </div>
+                            </li>
+                            <li
+                                class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                <div class="d-flex flex-column">
+                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">June, 25, 2019</h6>
+                                    <span class="text-xs">#QW-103578</span>
+                                </div>
+                                <div class="d-flex align-items-center text-sm">
+                                    {{-- $120 --}}
+                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
+                                            class="fas fa-file-pdf text-lg me-1"></i> CSV</button>
+                                </div>
+                            </li>
+                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
+                                <div class="d-flex flex-column">
+                                    <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2019</h6>
+                                    <span class="text-xs">#AR-803481</span>
+                                </div>
+                                <div class="d-flex align-items-center text-sm">
+                                    {{-- $300 --}}
+                                    <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
+                                            class="fas fa-file-pdf text-lg me-1"></i> CSV</button>
+                                </div>
+                            </li>
+                            <li class="list-group-item border-0 d-flex justify-content-between ps-0 border-radius-lg">
+                              <div class="d-flex flex-column">
+                                  <h6 class="text-dark mb-1 font-weight-bold text-sm">March, 01, 2019</h6>
+                                  <span class="text-xs">#ST-451897</span>
+                              </div>
+                              <div class="d-flex align-items-center text-sm">
+                                  {{-- $275 --}}
+                                  <button class="btn btn-link text-dark text-sm mb-0 px-0 ms-4"><i
+                                          class="fas fa-file-pdf text-lg me-1"></i> CSV</button>
+                              </div>
+                          </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
